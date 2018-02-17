c5493b5ea8eaf1b75876dd8bc987545bd3dd4b39
william-abboud <william_y_abboud@abv.bg>
2018-02-17 19:34:40 +0200

    Added initial working implementation for fixed width slides

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -7,15 +7,57 @@ class Slider extends Component {
 
     this.slideNext = this.slideNext.bind(this);
     this.slidePrevious = this.slidePrevious.bind(this);
+    this.getSliderRef = this.getSliderRef.bind(this);
+    this.initSlider = this.initSlider.bind(this);
 
     this.state = {
-      currentSlideIdx: 0
+      currentSlideIdx: 0,
+      style: {
+        display: "flex",
+        boxSizing: "border-box",
+        transition: "transform 0.5s ease-out",
+      },
     };
 
     this.api = {
       slideNext: this.slideNext,
       slidePrevious: this.slidePrevious,
     };
+  }
+
+  componentDidMount() {
+    this.initSlider();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { currentSlideIdx } = this.state;
+
+    if (prevState.currentSlideIdx !== currentSlideIdx) {
+      const slideWidth = this.slides[0].getBoundingClientRect().width;
+
+      this.setState({ style: {
+          ...this.state.style,
+          transform: `translateX(-${currentSlideIdx * slideWidth}px)`,
+        }
+      });
+    }
+  }
+
+  getSliderRef(ref) {
+    this.slider = ref;
+    this.slidesContainer = this.slider.children[0];
+    this.slides = this.slidesContainer.children;
+  }
+
+  initSlider() {
+    const slideWidth = this.slides[0].getBoundingClientRect().width;
+    const widthPx = `${slideWidth}px`;
+    const slides = [...this.slides];
+
+    slides.map(slide => slide.style.minWidth = widthPx);
+
+    this.slider.style.width = widthPx;
+    this.slider.style.overflow = "hidden";
   }
 
   slideNext() {
@@ -32,8 +74,10 @@ class Slider extends Component {
 
   render() {
     return (
-      <div className="slider">
-        { this.props.children({ ...this.state, ...this.api }) }
+      <div className="slider" ref={this.getSliderRef}>
+        <div className="slides-container" style={this.state.style}>
+          { this.props.children({ ...this.state, ...this.api }) }
+        </div>
       </div>
     );
   }
