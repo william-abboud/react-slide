50b0cba04d5536eae75497d3fe8fe808ff41fb72
william-abboud <william_y_abboud@abv.bg>
2018-02-18 12:26:12 +0200

    Added number labels to slides

--- src/js/slides.jsx
+++ src/js/slides.jsx
@@ -7,9 +7,9 @@ class Slides extends Component {
 
   render() {
     return [
-      <div style={{ width: "100px" }} onClick={this.props.slideNext}>Slide</div>,
-      <div style={{ width: "100px" }} onClick={this.props.slideNext}>Slide</div>,
-      <div style={{ width: "100px" }} onClick={this.props.slideNext}>Slide</div>,
+      <div style={{ width: "100px" }} onClick={this.props.slideNext}>Slide 1</div>,
+      <div style={{ width: "100px" }} onClick={this.props.slideNext}>Slide 2</div>,
+      <div style={{ width: "100px" }} onClick={this.props.slideNext}>Slide 3</div>,
     ];
   }
 }
