6f4944c6e41a83acf3cab5604140700f8526b7c4
william-abboud <william_y_abboud@abv.bg>
2018-02-18 12:26:51 +0200

    Mocked refs correctly, improved tests logic + more tests

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -6,34 +6,98 @@ describe("Slider", () => {
   let Child;
   let sliderInst;
   let childInst;
+  let options = {
+    createNodeMock(element) {
+      if (element.props.className === "slider") {
+        return {
+          style: {},
+          children: [
+            {
+              children: [
+                {
+                  getBoundingClientRect() {
+                    return { width: 100 };
+                  },
+                  style: {}
+                },
+                {
+                  getBoundingClientRect() {
+                    return { width: 100 };
+                  },
+                  style: {}
+                },
+                {
+                  getBoundingClientRect() {
+                    return { width: 100 };
+                  },
+                  style: {}
+                }
+              ]
+            }
+          ]
+        };
+      }
+
+      return null;
+    }
+  };
 
   beforeEach(() => {
-    Child = (props) => null;
+    Child = (props) => [
+      <div key="first">first slide</div>,
+      <div key="second">second slide</div>,
+      <div key="third">third slide</div>
+    ];
+
     sliderInst = renderer.create(
       <Slider>
         { (sliderProps) => <Child {...sliderProps} /> }
-      </Slider>
+      </Slider>,
+      options
     );
     childInst = sliderInst.root.findByType(Child);
   });
 
   test("passes current slide index to children", () => {
-    expect(childInst.props.currentSlideIdx).toBe(0);
+    expect(childInst.props.currentSlideIdx).toBeDefined();
   });
 
   test("goes to next slide", () => {
-    expect(childInst.props.currentSlideIdx).toBe(0);
+    const lastSlideIndex = childInst.props.currentSlideIdx;
 
     childInst.props.slideNext();
-    expect(childInst.props.currentSlideIdx).toBe(1);
+
+    const nextSlideIndex = childInst.props.currentSlideIdx;
+
+    expect(nextSlideIndex - lastSlideIndex).toBe(1);
   });
 
   test("goes to previous slide", () => {
+    const lastSlideIndex = childInst.props.currentSlideIdx;
+
     childInst.props.slideNext();
-    expect(childInst.props.currentSlideIdx).toBe(1);
+    childInst.props.slidePrevious();
+
+    const nextSlideIndex = childInst.props.currentSlideIdx;
+
+    expect(nextSlideIndex).toEqual(lastSlideIndex);
+  });
+
+  test("doesn't slide to previous slide when current slide is first slide", () => {
+    expect(childInst.props.currentSlideIdx).toBe(0);
 
     childInst.props.slidePrevious();
     expect(childInst.props.currentSlideIdx).toBe(0);
+  });
+
+  test("doesn't slide to next slide when current slide is last slide", () => {
+    expect(childInst.props.currentSlideIdx).toBe(0);
+
+    childInst.props.slideNext();
+    childInst.props.slideNext();
+    childInst.props.slideNext();
+
+    expect(childInst.props.currentSlideIdx).toBe(2);
   });
 });
 
